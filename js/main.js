2c265268b989d70ad628b87c3df98060f7a446c9
Rimantas <belauzas@yahoo.com>
2022-11-14 01:42:40 +0200

    Initial commit

--- js/main.js
+++ js/main.js
@@ -23,3 +23,77 @@ for (const themeOptionDOM of themeOptionsDOM) {
         updateTheme(themeOptionDOM.dataset.theme);
     })
 }
+
+const nameDOM = document.getElementById('name');
+const reasonDOM = document.getElementById('reason');
+const levelDOM = document.getElementById('level');
+const githubDOM = document.getElementById('github');
+const linkedinDOM = document.getElementById('linkedin');
+const submitDOM = document.getElementById('submit');
+
+const abc = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZąčęėįšųūžĄČĘĖĮŠŲŪŽ';
+
+function validSingleName(str, sentence = 'Vardas') {
+    const min = 3;
+    const max = 30;
+
+    if (word.length < min || word.length > max) {
+        return [false, `${sentence} turi būti tarp ${min} ir ${max} simbolių`];
+    }
+    if (word[0] !== word[0].toUpperCase()) {
+        return [false, `${sentence} turi prasidėti didžiąja raide 😬`];
+    }
+    if (word.slice(1) !== word.slice(1).toLowerCase()) {
+        return [false, `Sekančios ${sentence} raidės turi būti mažosios 😬`];
+    }
+
+    for (const s of str) {
+        if (!abc.includes(s)) {
+            return [false, `Varde neleistinas simbolis "${s}"`];
+        }
+    }
+
+    return [true, 'OK'];
+}
+
+function validName(str) {
+    str = str.trim();
+    const words = str.split(' ');
+
+    if (words.length === 1) {
+        const [err, msg] = validSingleName(word);
+        if (err) {
+            return [err, msg];
+        }
+    } else {
+        for (const word of words) {
+            const [err, msg] = validSingleName(word, 'vardo dalies');
+            if (err) {
+                return [err, msg];
+            }
+        }
+    }
+
+    return [true, 'OK'];
+}
+
+function validReasonLevel(str) {
+    const min = 5;
+    const max = 500;
+
+    str = str.trim();
+
+    if (str.length < min || str.length > max) {
+        return [false, `Aprašas turi būti tarp ${min} ir ${max} simbolių`];
+    }
+
+    return [true, 'OK'];
+}
+
+function validLink(str) {
+    str = str.trim();
+    if (false) {
+        return [false, 'Error'];
+    }
+    return [true, 'OK'];
+}
---
+++ notes.md
@@ -0,0 +1,37 @@
+# Design
+
+https://www.pinterest.com/pin/756182593687543514/
+https://www.pinterest.com/pin/787496684856180845/
+https://dribbble.com/shots/4050156-Dashboard-Fields
+https://dribbble.com/shots/5090322-Event-Planning-Wizard
+https://dribbble.com/shots/14360977-Multi-Step-Form-03
+
+# Questions
+
+1. Kuo vardu?
+    - input
+2. Koks tavo tikslas?
+    - siaip, apsidairyti
+    - mokytis programavimo, ieskoti mentoriaus
+    - padeti kitiems mokytis programavimo
+    - ieskoti potencialiu darbuotoju (HR)
+    - kita
+        - input
+3. Koks tavo lygis?
+    - nieko nemoku
+    - moku, bet mazai
+    - pakankamai moku
+    - daug moku
+    - moku viska
+4. Ar turi Github paskyra?
+    - taip
+        - input
+    - ne (susikurt (link'as) ir grizk pratesti registracija)
+5. Ar turi Linkedin paskyra?
+    - taip
+        - input
+    - ne (susikurt (link'as) ir grizk pratesti registracija)
+6. Ar turi Discord paskyra?
+    - taip
+    - ne (susikurt (link'as) ir grizk pratesti registracija)
+7. Sugeneruotas prisistatymo tekstas + INVITE LINK
